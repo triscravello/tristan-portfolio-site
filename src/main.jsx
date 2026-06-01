a0d064bab4b09fe89a6ef94c6381889be8474417
Tristan Cravello <tlcravello@gmail.com>
2026-05-31 22:14:13 -0400

    Update config and basename with conditional logic for GitHub pages and S3

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const isGithubPages = import.meta.env.MODE === 'github';
+
+const basename = isGithubPages ? '/tristan-portfolio-site' : '';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename='/tristan-portfolio-site'>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/tristan-portfolio-site',
+export default defineConfig(({ mode }) => {
+  const isGithubPages = mode === 'github'
+
+  return {
+    plugins: [react()],
+    base: isGithubPages ? '/tristan-portfolio-site' : '/',
+  }
 })
