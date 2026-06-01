37872be74198ae1a0dfc8f6568f2d39eefe73798
Tristan Cravello <tlcravello@gmail.com>
2026-05-31 22:02:52 -0400

    Fix basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter basename='/tristan-portfolio-site'>
     <App />
   </BrowserRouter>
 );
